c660e1327e3680285e0ddd58978da33d76d39f35
Json0713 <jason.orsuabayaga13@gmail.com>
2025-12-29 23:06:57 +0800

    Update-vercel-path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,12 @@ import { Routes } from '@angular/router';
 import { Index } from './public/index';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    component: Index
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
