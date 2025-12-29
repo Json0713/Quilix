54460e30ae115afcf2cd6eba3339acefdd651306
Json0713 <jason.orsuabayaga13@gmail.com>
2025-12-29 21:34:46 +0800

    test-route-001

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,5 @@ import { Routes } from '@angular/router';
 import { Index } from './public/index';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: Index
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+
 ];
