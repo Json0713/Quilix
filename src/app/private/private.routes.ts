04b8c83ea854f921ae0b37e83e982925e85630ad
Json0713 <jason.orsuabayaga13@gmail.com>
2026-01-04 21:18:26 +0800

    aut-guard-rename

--- src/app/private/private.routes.ts
+++ src/app/private/private.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { authGuard } from '../core/auth/auth.guard';
 import { STUDENT_ROUTES } from './pages/student/student.routes';
 import { TEACHER_ROUTES } from './pages/teacher/teacher.routes';
-import { roleGuard } from '../core/guards/role-guard';
+import { roleGuard } from '../core/guards/role.guard';
 
 export const PRIVATE_ROUTES: Routes = [
   {
