0537c24251dd1c9316057b3676d1d93b9c403c11
Json0713 <jason.orsuabayaga13@gmail.com>
2026-01-05 21:41:47 +0800

    facade-integration

---
+++ src/app/core/storage/storage.facade.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Storage } from './storage';
+import { User } from '../interfaces/user';
+import { Session } from '../interfaces/session';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageFacade {
+
+
+  constructor(private storage: Storage) {}
+
+  /* USERS (LocalStorage) */
+  getUsers(): User[] {
+    return this.storage.getUsers();
+  }
+
+  getUserById(id: string): User | null {
+    return this.storage.getUserById(id);
+  }
+
+  saveUsers(users: User[]): void {
+    this.storage.saveUsers(users);
+  }
+
+  addUser(user: User): void {
+    this.storage.addUser(user);
+  }
+
+  removeUser(userId: string): void {
+    this.storage.removeUser(userId);
+  }
+
+  updateUserLastActive(userId: string): void {
+    this.storage.updateUserLastActive(userId);
+  }
+
+  /* SESSION (LocalStorage) */
+  saveSession(session: Session): void {
+    this.storage.saveSession(session);
+  }
+
+  getSession(): Session | null {
+    return this.storage.getSession();
+  }
+
+  clearSession(): void {
+    this.storage.clearSession();
+  }
+  
+}
