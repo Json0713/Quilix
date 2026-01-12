c758bf06dd7da13b78088c899ce78eb66636f34a
Json0713 <jason.orsuabayaga13@gmail.com>
2026-01-12 21:48:58 +0800

    public-optimized-loader

--- src/app/public/template/template.ts
+++ src/app/public/template/template.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { Loader } from '../../shared/ui/common/loader/loader';
 import { Toast } from '../../shared/ui/common/toast/toast';
 import { Modal } from '../../shared/ui/common/modal/modal';
 
 @Component({
   selector: 'app-template',
-  imports: [RouterOutlet, Toast, Modal],
+  imports: [RouterOutlet, Loader, Toast, Modal],
   templateUrl: './template.html',
   styleUrl: './template.scss',
 })
