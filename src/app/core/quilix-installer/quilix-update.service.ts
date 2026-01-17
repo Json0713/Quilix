3fdf4b82d9441fce91646b7ec84e7f86d7a35c61
Json0713 <jason.orsuabayaga13@gmail.com>
2026-01-17 18:06:34 +0800

    refine-update-service

--- src/app/core/quilix-installer/quilix-update.service.ts
+++ src/app/core/quilix-installer/quilix-update.service.ts
@@ -1,34 +1,56 @@
-import { Injectable, inject } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
-import { ToastRelayService } from '../../services/ui/common/toast/toast-relay';
+import { Injectable, inject, OnDestroy } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { Subject, filter, takeUntil } from 'rxjs';
 
+import { ToastRelayService } from '../../services/ui/common/toast/toast-relay';
 
 @Injectable({ providedIn: 'root' 
 })
-export class QuilixUpdateService {
+export class QuilixUpdateService implements OnDestroy {
 
-  private swUpdate = inject(SwUpdate);
-  private toastRelay = inject(ToastRelayService);
+  private readonly swUpdate = inject(SwUpdate);
+  private readonly toastRelay = inject(ToastRelayService);
+
+  // UI/UX Hygiene 
+  private readonly destroy$ = new Subject<void>();
+
+  // UX Timing
+  private static readonly RELOAD_DELAY_MS = 1500;
+  private static readonly TOAST_DURATION_MS = 32000;
 
   init(): void {
     if (!this.swUpdate.isEnabled) return;
 
-    this.swUpdate.versionUpdates.subscribe(event => {
-      if (event.type === 'VERSION_READY') {
-        this.toastRelay.set(
-          'info',
-          'New Version Detected.\n' +
-          'Your App is now Updated.',
-          32000
-        )
+    this.swUpdate.versionUpdates
+      .pipe(
+        filter(
+          (event): event is VersionReadyEvent =>
+            event.type === 'VERSION_READY'
+        ),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => this.handleVersionReady());
+  }
 
-        setTimeout(() => {
-          this.swUpdate.activateUpdate().then(() => {
-            location.reload();
-          });
-        }, 1500);
-      }
+  private handleVersionReady(): void {
+    this.toastRelay.set(
+      'info',
+      'New version detected.\nYour app is now updating.',
+      QuilixUpdateService.TOAST_DURATION_MS
+    );
+
+    setTimeout(() => this.activateAndReload(), QuilixUpdateService.RELOAD_DELAY_MS);
+  }
+
+  private activateAndReload(): void {
+    this.swUpdate.activateUpdate().then(() => {
+      location.reload();
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
