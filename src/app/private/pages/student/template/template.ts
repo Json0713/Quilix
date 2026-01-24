1a5d4337abc8257a3a264fdd46c574785b07fdb9
Json0713 <jason.orsuabayaga13@gmail.com>
2026-01-24 08:10:53 +0800

    clean-sanitized-cpnt-temp

--- src/app/private/pages/student/template/template.ts
+++ src/app/private/pages/student/template/template.ts
@@ -1,15 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Loader } from '../../../../shared/ui/common/loader/loader';
-import { Toast } from '../../../../shared/ui/common/toast/toast';
-import { Modal } from "../../../../shared/ui/common/modal/modal";
 
 import { OsNotificationService } from '../../../../core/notifications/os-notification.service';
 
 
 @Component({
   selector: 'app-student-template',
-  imports: [RouterOutlet, Loader, Toast, Modal],
+  imports: [RouterOutlet],
   templateUrl: './template.html',
   styleUrl: './template.scss',
 })
