5124b8ca9e95baf9442cb36d74fe024f1c38ea06
Json0713 <jason.orsuabayaga13@gmail.com>
2026-02-14 00:46:30 +0800

    clean-meata-guards

--- src/app/meta/private/private-meta.routes.ts
+++ src/app/meta/private/private-meta.routes.ts
@@ -3,19 +3,18 @@ import { Routes } from '@angular/router';
 import { PERSONAL_META_ROUTES } from './pages/personal-meta/personal-meta.routes';
 import { TEAM_META_ROUTES } from './pages/team-meta/team-meta.routes';
 import { MetaRoleGuard } from '../core/guards/meta-role.guard';
-import { MetaAuthGuard } from '../core/guards/meta-auth.guard';
 
 export const PRIVATE_META_ROUTES: Routes = [
 
   {
     path: 'personal',
-    canActivate: [MetaAuthGuard, MetaRoleGuard],
+    canActivate: [MetaRoleGuard],
     data: { role: 'personal' },
     children: PERSONAL_META_ROUTES
   },
   {
     path: 'team',
-    canActivate: [MetaAuthGuard, MetaRoleGuard],
+    canActivate: [MetaRoleGuard],
     data: { role: 'team' },
     children: TEAM_META_ROUTES
   }
