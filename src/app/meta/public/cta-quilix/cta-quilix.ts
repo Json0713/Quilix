ff2c755dbd8372173536ce50d2382476b4ba11b9
Json0713 <jason.orsuabayaga13@gmail.com>
2026-02-21 22:27:49 +0800

    meta-landing-01

---
+++ src/app/meta/public/cta-quilix/cta-quilix.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-cta-quilix',
+    standalone: true,
+    imports: [RouterModule],
+    templateUrl: './cta-quilix.html',
+    styleUrl: './cta-quilix.scss',
+})
+export class CtaQuilix { }
--- src/app/meta/public/index/index.ts
+++ src/app/meta/public/index/index.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { QuilixVersionService } from '../../../core/quilix-installer/version/quilix-version.service';
+import { CtaQuilix } from '../cta-quilix/cta-quilix';
 
 @Component({
   selector: 'public-meta-index',
-  imports: [RouterModule],
+  imports: [RouterModule, CtaQuilix],
   templateUrl: './index.html',
   styleUrl: './index.scss',
 })
