73575450cab4c144255f02d8db7a0531c51e1832
Json0713 <jason.orsuabayaga13@gmail.com>
2026-05-09 20:02:47 +0800

    refine-space-settings

--- src/app/shared/components/space-settings/space-settings.ts
+++ src/app/shared/components/space-settings/space-settings.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
+import { Component, Input, Output, EventEmitter, signal, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SpacePreferences, DEFAULT_SPACE_PREFERENCES } from '../../../core/interfaces/space-preferences';
@@ -10,11 +10,18 @@ import { SpacePreferences, DEFAULT_SPACE_PREFERENCES } from '../../../core/inter
     templateUrl: './space-settings.html',
     styleUrl: './space-settings.scss'
 })
-export class SpaceSettingsComponent {
+export class SpaceSettingsComponent implements OnChanges {
     @Input() preferences: SpacePreferences = { ...DEFAULT_SPACE_PREFERENCES };
+    @Input() isVisible = false;
     @Output() preferencesChange = new EventEmitter<SpacePreferences>();
 
     currentView = signal<'menu' | 'general' | 'appearance'>('menu');
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['isVisible'] && changes['isVisible'].currentValue) {
+            this.currentView.set('menu');
+        }
+    }
 
     navigateTo(view: 'menu' | 'general' | 'appearance') {
         this.currentView.set(view);
